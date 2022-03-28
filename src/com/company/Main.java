41c60e4dbdab2da8ecdc400e61498cab25ad2a8e
Sergey Makarevich <94475773+iddqd000@users.noreply.github.com>
2022-03-28 22:01:35 +0300

    removed extra files, added new files

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MyTriangle z = new MyTriangle(0,0,1,0,0,1);
+        System.out.print(z.getType());
+        System.out.print(z.getPerimetr());
+    }
+
+}
+
